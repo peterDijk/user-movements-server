b24ea32c48aa0c944eeaa134aaf0121c933a07a4
Peter van Dijk <peter.van.dijk@bynder.com>
2023-03-28 11:29:40 +0200

    dockerized and app connects to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MovementsModule } from './movements/movements.module';
+
+const mongoDbUri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_CONTAINER_PORT}/?retryWrites=true&w=majority`;
+
+console.log({ mongoDbUri });
 
 @Module({
-  imports: [],
+  imports: [
+    MovementsModule,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: mongoDbUri,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
