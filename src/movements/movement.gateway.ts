0ffb96dad5a4c2fcd5398412ad17e27507f60ea7
Peter van Dijk <peter.van.dijk@bynder.com>
2023-03-28 15:52:23 +0200

    incoming message data is stored in repository

--- src/movements/movement.gateway.ts
+++ src/movements/movement.gateway.ts
@@ -6,10 +6,13 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
+import { MovementsService } from './movements.service';
 
 @WebSocketGateway()
 export class MovementGateway {
   private logger = new Logger(MovementGateway.name);
+
+  constructor(private readonly movementsService: MovementsService) {}
 
   @WebSocketServer()
   server: Server;
@@ -18,7 +21,10 @@ export class MovementGateway {
   handleMovement(
     @MessageBody() data: { userId: string; mouseX: number; mouseY: number },
   ) {
-    this.logger.log('incoming message "new_movement"', { userId: data.userId });
-    this.server.sockets.emit('receive_message', data);
+    this.logger.log(
+      `incoming message "new_movement" from userId: ${data.userId}`,
+    );
+    this.movementsService.create(data);
+    this.server.sockets.emit('new_movement_received', data);
   }
 }
--- src/movements/movements.service.ts
+++ src/movements/movements.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
 import { Movement } from './entities/movement.entity';
 
 @Injectable()
 export class MovementsService {
+  private logger = new Logger(MovementsService.name);
+
   constructor(
     @InjectRepository(Movement)
     private movementRepository: MongoRepository<Movement>,
   ) {}
 
   create(data: { userId: string; mouseX: number; mouseY: number }) {
+    this.logger.log(`incoming data to save: ${data}`);
     if (data.userId && data.mouseX && data.mouseY) {
       return this.movementRepository.save(data);
     }
