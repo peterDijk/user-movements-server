ce278e2166b00d7a584bf7ed5dc2ba6fc931dae1
Peter van Dijk <peter.van.dijk@bynder.com>
2023-03-28 12:55:47 +0200

    creating mongo user on init docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovementsModule } from './movements/movements.module';
 
-const mongoDbUri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_CONTAINER_PORT}/?retryWrites=true&w=majority`;
+const mongoDbUri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_CONTAINER_PORT}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
-console.log({ mongoDbUri });
+// console.log({ mongoDbUri });
 
 @Module({
   imports: [
     MovementsModule,
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: mongoDbUri,
+      // url: mongoDbUri,
+      host: process.env.MONGO_HOST,
+      port: parseInt(process.env.MONGO_CONTAINER_PORT),
+      username: process.env.MONGO_USER,
+      password: process.env.MONGO_PASSWORD,
+      database: process.env.MONGO_DB,
+      useUnifiedTopology: true,
     }),
   ],
   controllers: [AppController],
