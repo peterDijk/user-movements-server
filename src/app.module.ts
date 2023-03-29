64a0aa551133acda274e81e48c55f7256574d4df
Peter van Dijk <peter.van.dijk@bynder.com>
2023-03-29 08:59:35 +0200

    connecting setting authSource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovementsModule } from './movements/movements.module';
 
+const mongoUrl = `mongodb://admin:admin@movements-db:27016/user-movements?authSource=admin`;
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
+      // url: mongoUrl,
       host: 'movements-db',
       port: 27016,
       username: 'admin',
       password: 'admin',
-      // database: 'user-movements',
+      database: 'user-movements',
+      authSource: 'admin',
       useUnifiedTopology: true,
       synchronize: true,
       logger: 'debug',
